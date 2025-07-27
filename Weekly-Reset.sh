41b90f344e9ecb6670d73db3850fe3720ead2fdd
Royal-Terrapin <132233072+Royal-Terrapin@users.noreply.github.com>
2025-07-26 22:33:04 -0700

    Update Weekly-Reset.sh
    need to create dynamic.txt
    need to implement sed for variables in dynamic.txt

--- Weekly-Reset.sh
+++ Weekly-Reset.sh
@@ -1,9 +1,15 @@
 source config.txt
+source dynamic.txt
 
 for PLAYER in ${PLAYERS[@]}
 do
   TIMEPLAYED=TIMEPLAYED_$PLAYER
-  ${!TIMEPLAYED}=0
+  declare $TIMEPLAYED=0
+# the player hasn't played at all this week
   LIMITMET=LIMITMET_$PLAYER
-  ${!LIMITMET}=0
+  declare $LIMITMET=0
+# the player hasn't met their limit
+  declare -p $TIMEPLAYED >> dynamic.txt
+  declare -p $LIMITMET >> dynamic.txt
+# save these values for later
 done
